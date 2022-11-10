55eed2c3815345662aa14315af833eaa127cd700
adrianvalido <adrianvalidofilm@gmail.com>
2022-11-10 09:41:57 -0500

    added accordion to beer list

--- frontend/src/Components/Beer/BeerCard.js
+++ frontend/src/Components/Beer/BeerCard.js
@@ -2,21 +2,40 @@ import React from "react";
 
 
 export default function BeerList(props) {
+
+    const[selected, setSelected] = React.useState(false)
+
+    function toggle(){
+        setSelected(oldSelect => !oldSelect)
+    }
+
     //TODO:
     //able to arrange list by rating? alphabet? 
 
         //fetch: get all beers
 return(
-    <div>
-    <a href={props.beerLink} className="beercard">
+    <div className="beercard">
             <img src={props.beerImage} className='cardImage'/>
             <div className="beercard-details">
                 <h1>{props.beerName}</h1>
                 <h4 className="beerType">Type: {props.beerType}</h4> 
                 <h4 className="beerDesc">{props.beerDesc}</h4>
                 <h3>Rating: {props.beerRating}</h3>
+                <div className="accordion">
+                <div className={selected ? 'info show' : 'info'}>
+                        <div>Hi</div>
+                        <div>Hello</div>
+                        <div>Howdy</div>
+                        <div>Howareya</div>
+                        </div>
+                    <div className="title" onClick={toggle}>
+                        <h6>{selected ? 'Show less...' : 'See more...'}</h6>
+                        
+                    </div>
+            
+                </div>
             </div>
-            </a>
+            
     </div>    
 )
 }
