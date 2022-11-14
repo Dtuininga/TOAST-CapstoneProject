37209b5924e5e2d95b2b97b26e34b03cc1c836c5
mensahTribeWeb <ndm.acct85@gmail.com>
2022-11-14 00:11:40 -0600

    searchbar setup near complete working on logic

--- frontend/src/Components/SearchBar/SearchBar.js
+++ frontend/src/Components/SearchBar/SearchBar.js
@@ -1,9 +1,20 @@
 import React from "react";
+import {baseUrl} from '../../Shared/baseUrl'
 
-export default function(){
+const SearchBar = ({searchChange})=>{
+  
   return(
-    <div className="search">
-      <input placeholder="Search" />
-    </div>
+    <form className="searchBar--form">
+      <input 
+      className="searchBar--form--input" 
+      placeholder="Search"
+      type="search" 
+      // name={}
+      // value={}
+      onChange={searchChange}
+      />
+      <button className="searchBar--button"></button>
+    </form>
   );
 }
+export default SearchBar;
