239172845ac2d162fd2690181a5200a85f3d45ce
mensahTribeWeb <ndm.acct85@gmail.com>
2022-11-16 13:52:29 -0600

    working on react logic

--- frontend/src/Components/SearchBar/SearchBar.js
+++ frontend/src/Components/SearchBar/SearchBar.js
@@ -1,8 +1,17 @@
-import React from "react";
+// vendor libraries
+import React, {useState} from "react";
 import {baseUrl} from '../../Shared/baseUrl'
 
-const SearchBar = ({searchChange})=>{
-  
+// Styles
+import '../SearchBar/search-img/search.css'
+
+//place components to search later
+
+export default function SearchBar (){
+
+   const [query, setQuery] = useState('');
+   const [brewery, setBrewery] = useState([]);
+
   return(
     <div className="container">
       <header className="search-header">
@@ -10,6 +19,7 @@ const SearchBar = ({searchChange})=>{
         <a href="" className="href">Brewery watchlist</a>
       </header>
     <form className="searchBar--form">
+      <label htmlFor=""></label>
       <div className="search-bar-icon" id="search-bar-icon">
         <img src="./search-img/icon-sb.png" alt="search icon" />
       </div>
@@ -17,20 +27,20 @@ const SearchBar = ({searchChange})=>{
           className="searchBar--form--input" 
           placeholder="Lets search for a brewery"
           type="search" 
-          // name={}
+          name="query"
           // value={}
-          onChange={searchChange}
+          onChange={(e) => setQuery(e.target.value)}
       />
       <button title="search-btn"
               className="searchBar--button" 
-              id="search--btn" >  
+              id="search--btn" > 
+              Submit 
               </button>
     </form>
-    
-    <section id="brewery--searched" className="brewery--searched">
-    </section>
+
+    {/* <section id="brewery--searched" className="brewery--searched">
+    </section> */}
 
     </div>
   );
 }
-export default SearchBar;
