22f0d34cc9ca900410b6ac4215412828f3fa4089
mensahTribeWeb <ndm.acct85@gmail.com>
2022-11-16 12:54:37 -0600

    JSX semantics done

--- frontend/src/Components/SearchBar/SearchBar.js
+++ frontend/src/Components/SearchBar/SearchBar.js
@@ -4,17 +4,32 @@ import {baseUrl} from '../../Shared/baseUrl'
 const SearchBar = ({searchChange})=>{
   
   return(
+    <div className="container">
+      <header>
+        <h1 className="title">Lets find a brewery to Toast to!</h1>
+        <a href="" className="href">Brewery watchlist</a>
+      </header>
     <form className="searchBar--form">
+      <div className="search-bar-icon" id="search-bar-icon">
+        <img src="./search-img/icon-sb.png" alt="search icon" />
+      </div>
       <input 
-      className="searchBar--form--input" 
-      placeholder="Search"
-      type="search" 
-      // name={}
-      // value={}
-      onChange={searchChange}
+          className="searchBar--form--input" 
+          placeholder="Lets search for a brewery"
+          type="search" 
+          // name={}
+          // value={}
+          onChange={searchChange}
       />
-      <button className="searchBar--button"></button>
+      <button title="search--btn"
+              className="searchBar--button" 
+              id="search--btn" >  
+              </button>
     </form>
+    <section id="brewery--searched" className="brewery--searched">
+    </section>
+
+    </div>
   );
 }
 export default SearchBar;
