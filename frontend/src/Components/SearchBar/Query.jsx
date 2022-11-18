505a206e66fa08c3bc5b0c055d48f577f37bacfe
mensahTribeWeb <ndm.acct85@gmail.com>
2022-11-18 00:13:30 -0600

    add useEffect func and dependancy array to prevent looped rendering

--- frontend/src/Components/SearchBar/Query.jsx
+++ frontend/src/Components/SearchBar/Query.jsx
@@ -1,17 +1,24 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import BreweryData from "./BreweryData";
 import { baseUrl } from '../../Shared/baseUrl';
 import { useStore } from 'react-redux';
 import {Link} from 'react-router-dom';
 
-
-export default function Query() {
+export default function Query(props) {
   // states to attribute
+  const token = store.getState().token.token
   const [queryData, setQueryData] = useState({BreweryData});
   console.log(queryData);
 
   const [results, setResults] =  useState([]);
 
+  //Use Effect
+useEffect(() => {
+  fetch(`http://localhost:8081/breweriesbyuser/ + ${props.userId}`, {headers: {'Authorization' : 'Bearer ' + token}})
+  .then(res => {res.json()})
+  .then(results => setQueryData(results))
+}, [props])
+ 
   // functions
 
   //input field value change
@@ -34,6 +41,7 @@ const handleSubmit = (e) => {
 } 
   return (
           <form onSubmit={handleSubmit}>
+            <pre>{JSON.stringify(queryData, null, 2)}</pre>
             <input 
               className="query" placeholder="Lets search for a brewery" type="text"
               value={queryData} 
